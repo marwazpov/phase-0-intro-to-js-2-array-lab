d438029cf0d9152bf9db68ec5901f8e36c671280
marwa <mds08@mail.aub.edu>
2022-08-05 23:17:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob')
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat() {
+    return [...cats,'Broom'];
+}
+
+function prependCat(name){
+    return ['Arnold', ...cats];
+}
+
+function removeLastCat() {
+    removeLastCat = cats.slice(0, cats.length - 1)
+    return removeLastCat
+}
+
+function removeFirstCat() {
+    removeFirstCat = cats.slice(1)
+    return removeFirstCat
+}
